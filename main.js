bf1deec29a0b45d03bf81a925f75b0312a03a827
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 02:43:19 +0100

    test

--- main.js
+++ main.js
@@ -5,20 +5,22 @@ bot.login(process.env.token);
 
 bot.on("ready", () => {
     console.log("Bot activated. Ready to scream"); 
-    var curDate = new Date(),
-    curday = curDate.getDay();
+    var curDate = new Date();
+    while(true) {
+        var curday = curDate.getDay();
 
-    console.log(curday);
+        console.log(curday);
 
-    const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
-    const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
-    // var ourchannel = Discord.channel.id("764604038271467553");
+        const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
+        const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
+        // var ourchannel = Discord.channel.id("764604038271467553");
 
 
-    if(curday == 3) {   
-        console.log("It's wednesday my Dudes");
-        test.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
-            files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
-        });
+        if(curday == 3) {   
+            console.log("It's wednesday my Dudes");
+            test.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
+                files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
+            });
+        }
     }
 });
