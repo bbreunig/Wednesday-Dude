ecc4b71076555e653447db678829a79a144b9c5f
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 21:25:26 +0100

    Testing smthing

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 const Discord = require('discord.js');
+const date = require("dayjs");
 const bot = new Discord.Client();
-
-bot.login(process.env.token);
 
 bot.on("ready", () => {
     console.log("Bot activated. Ready to scream"); 
-    var curDate = new Date();
-    // while(true) {
-        var curday = curDate.getDay();
+    var curDate = dayjs();
+     while(curDate.seconds().isValid()) {
 
-        console.log(curday);
+        console.log(curDate.day());
 
         const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
         const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
         // var ourchannel = Discord.channel.id("764604038271467553");
 
 
-        if(curday == 3) {   
+        if(curDate.day() == 3) {   
             setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
             test.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
                 files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
             });
         }
-    // }
+    }
 });
+
+bot.login(process.env.token);
