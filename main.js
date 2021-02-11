428e7ad08c93a79d0a08158cc03a8091b23229f6
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 22:09:47 +0100

    should write wednesday my dudes at 00:00

--- main.js
+++ main.js
@@ -2,27 +2,24 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-// var nextwdn =  
+
 
 function checkWdn() {
     var date = new Date();
     const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
-    if(date.getDay() == 3) {   
+    if(date.getDay() === 3 && date.getSeconds() === 0 && date.getMinutes() === 0 && date.getHours() === 0) {   
         setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
         channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
             files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
         });
     } else {
-        console.log(date.getSeconds());
+        console.log(date.getMinutes + ":" + date.getSeconds());
     }
     setTimeout(checkWdn, 1000);
 }
 
 bot.on("ready", () => {
     console.log("Bot activated. Ready to scream"); 
-    
-    console.log();
-    
     
     const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
     // var ourchannel = Discord.channel.id("764604038271467553");
