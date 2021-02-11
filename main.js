1dcec6d0cff0ee5448d1ec6a1023161a71eabefa
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 23:05:56 +0100

    try this

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ bot.on('message', message => {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        console.log("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
         message.channel.send("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
     }
 });
