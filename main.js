ad6679a1c866773f2a45e446d15e51c2c16b0e81
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 21:57:21 +0100

    still have to fix channels and countdown

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 const Discord = require('discord.js');
-var date = require("dayjs");
-date().format();
 const bot = new Discord.Client();
+
+var date = new Date();
+// var nextwdn =  
+
+function checkWdn() {
+    const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
+    if(date.getDay() == 3) {   
+        setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
+        channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
+            files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
+        });
+    }
+    setTimeout(checkWdn, 1000);
+}
 
 bot.on("ready", () => {
     console.log("Bot activated. Ready to scream"); 
-     while(date().seconds().isValid()) {
-
-        console.log(date().day());
-
-        const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
-        const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
-        // var ourchannel = Discord.channel.id("764604038271467553");
-
-
-        if(date().day() == 3) {   
-            setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
-            test.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
-                files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
-            });
-        }
-    }
+    
+    console.log();
+    
+    
+    const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
+    // var ourchannel = Discord.channel.id("764604038271467553");
+    
+    checkWdn();
 });
 
 bot.login(process.env.token);
