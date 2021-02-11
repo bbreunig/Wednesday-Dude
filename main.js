bdc813cdb0faec3399649170175121bf5eca1d8e
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 21:29:47 +0100

    Test

--- main.js
+++ main.js
@@ -4,17 +4,16 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {
     console.log("Bot activated. Ready to scream"); 
-    var curDate = dayjs();
-     while(curDate.seconds().isValid()) {
+     while(date.seconds().isValid()) {
 
-        console.log(curDate.day());
+        console.log(date.day());
 
         const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
         const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
         // var ourchannel = Discord.channel.id("764604038271467553");
 
 
-        if(curDate.day() == 3) {   
+        if(date.day() == 3) {   
             setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
             test.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
                 files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
