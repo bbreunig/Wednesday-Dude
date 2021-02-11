8330fe9f8e5c9ceb23cc0f8ccb24531ef65bd3a1
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 23:13:27 +0100

    now with dudette

--- main.js
+++ main.js
@@ -33,6 +33,26 @@ bot.on('message', message => {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         // console.log("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
         message.channel.send("Next wednesday will be in " + days + "days " + hours + "hours " + minutes + "minutes and " + seconds + "seconds");
+    } else if (message.content.toLowerCase() === '!dudette') {
+        // console.log("If clause fails me");
+        var current = new Date(),
+        nextwdn = new Date();
+        while(nextwdn.getDay() != 3) {
+            var nextday = nextwdn.getDate()+1;
+            nextwdn.setDate(nextday);
+        }
+        nextwdn.setHours(0);
+        nextwdn.setMinutes(0);
+        nextwdn.setSeconds(0);
+
+        var distance = nextwdn - current;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // console.log("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
+        message.channel.send("Next wednesday will be in " + days + "days " + hours + "hours " + minutes + "minutes and " + seconds + "seconds UwU");
     }
 });
 
