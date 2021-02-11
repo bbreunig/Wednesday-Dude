a7173b2cc4e6dac13008cf0ba0bc7404c69f0dfb
ru92pod <B.Breunig@campus.lmu.de>
2021-02-11 22:52:33 +0100

    pls work

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ const bot = new Discord.Client();
 function checkWdn() {
     var date = new Date();
     const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
-    if(date.getDay() === 3 && date.getSeconds() === 0 && date.getMinutes() === 0 && date.getHours() === 0) {   
-        setTimeout(() => console.log("It's wednesday my Dudes"), 1000);
+    if((date.getDay() === 3) && (date.getSeconds() === 0) && (date.getMinutes() === 0) && (date.getHours() === 0)) {   
         channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
             files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
         });
@@ -27,23 +26,23 @@ bot.on("ready", () => {
     checkWdn();
 });
 
-// bot.on("message", message => {
-//     if (message === "!dude") {
-//         var current = new Date(),
-//         nextwdn = new Date();
-//         while(nextwdn.getDay != 3) {
-//             var nextday = nextwdn.getDate()+1;
-//             nextwdn.setDate(nextday);
-//         }
+bot.on("message", message => {
+    if (message === "!dude") {
+        var current = new Date(),
+        nextwdn = new Date();
+        while(nextwdn.getDay != 3) {
+            var nextday = nextwdn.getDate()+1;
+            nextwdn.setDate(nextday);
+        }
 
-//         var distance = nextwdn - current;
+        var distance = nextwdn - current;
 
-//         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//         message.channel.send("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
-//     }
-// });
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        message.channel.send("Next wednesday will be in " + days + ":" + hours + ":" + minutes + ":" + seconds);
+    }
+});
 
 bot.login(process.env.token);
