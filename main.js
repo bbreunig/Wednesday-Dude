ab182b0a36bbb42e9d5ee9c6a58e08972e800549
ru92pod <B.Breunig@campus.lmu.de>
2021-02-12 17:45:22 +0100

    pls work

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ bot.on("ready", () => {
     
     const test = bot.channels.cache.find(channel => channel.id === "428856232485453825");
     // var ourchannel = Discord.channel.id("764604038271467553");
-    
-    sleep(2000).then(() => {  
-            var date = new Date();
-            const channel = bot.channels.cache.find(channel => channel.createdTimestamp === Discord.Guild.createdTimestamp);
-        if((date.getDay() === 3) && (date.getSeconds() === 0) && (date.getMinutes() === 0) && (date.getHours() === 0)) {   
-            channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
-                files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
-            });
-        }
-        if(date.getMinutes() === 42) {
-            channel.send("It's just a test");
-            console.log("sending to channel: " + channel);
-        }
-        console.log("pls Timeout");
-    });
-
+    while(true) {
+        sleep(2000).then(() => {  
+                var date = new Date();
+                const channel = bot.channels.cache.find(channel => channel.createdTimestamp === Discord.Guild.createdTimestamp);
+            if((date.getDay() === 3) && (date.getSeconds() === 0) && (date.getMinutes() === 0) && (date.getHours() === 0)) {   
+                channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
+                    files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
+                });
+            }
+            if(date.getMinutes() === 47) {
+                channel.send("It's just a test");
+                console.log("sending to channel: " + channel);
+            }
+            console.log("pls Timeout");
+        });
+    }
 });
 
 bot.on('message', message => {
