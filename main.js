ee268657201102158872e6a4022720104a0edaa1
bbreunig <benjamin.breunig@icloud.com>
2021-02-12 22:21:57 +0100

    trying to fix wrong countdown

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ bot.on("ready", () => {
         var date = new Date();
         const channel = bot.channels.cache.find(channel => channel.id === "764604038271467553");
 
-        if((date.getDay() === 3) && (date.getSeconds() === 0) && (date.getMinutes() === 0) && (date.getHours() === 0)) {   
+        if((date.getDay() === 3) && (date.getSeconds() === 0) && (date.getMinutes() === 0) && (date.getHours() === 23)) {   
             channel.send("It's wednesday my dudes. AAAAAAAAAAAHHH.", {
                 files: ['https://cdn.discordapp.com/attachments/764604038271467553/809079104854425690/image0-1.jpg']
             });
@@ -31,7 +31,7 @@ bot.on('message', message => {
         var distance = nextwdn - current;
 
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + 1;
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         message.channel.send("Next wednesday will be in " + days + "days " + hours + "hours " + minutes + "minutes and " + seconds + "seconds");
